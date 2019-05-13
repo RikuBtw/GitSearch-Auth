97f71544d3bb2e39624bb5af71cac3fc5ac61f74
Julien Aubert <j.aubert@pepperbay.fr>
2019-05-13 21:08:12 +0200

    feat: logout

--- server.js
+++ server.js
@@ -9,8 +9,6 @@ const session = require('express-session');
 const request = require('request');
 
 const port = process.env.PORT || 5000;
-const csrf_string = randomString.generate();
-let access_token = null;
 
 app.use(express.static('views'));
 
@@ -21,31 +19,47 @@ app.use((req, res, next) => {
     next();
 });
 
+app.use(
+    session({
+        secret: randomString.generate(),
+        cookie: { maxAge: 60000 },
+        resave: false,
+        saveUninitialized: false
+    })
+);
+
 app.get('/', (req, res, next) => {
-    if (!access_token){
+    if (!req.session.access_token){
         res.status(403).json({ error: 'Please log in' });
         return;
     }
-    res.status(200).send([{'token': access_token}]);
+    res.status(200).send([{'token': req.session.access_token}]);
 });
 
 app.get('/login', (req, res, next) => {
+    req.session.csrf_string = randomString.generate();
     const githubAuthUrl =
     'https://github.com/login/oauth/authorize?' +
     qs.stringify({
         client_id: process.env.CLIENT_ID,
         redirect_uri: process.env.HOST + '/redirect',
-        state: csrf_string,
+        state: req.session.csrf_string,
         scope: 'repo user read:org'
     });
     
     res.redirect(githubAuthUrl);
 });
 
+app.get('/logout', (req, res, next) => {
+    req.session.access_token = null;
+    res.cookie('access_token', req.session.access_token, { maxAge: 0, httpOnly: true });
+    res.redirect(process.env.CLIENT_HOST);
+});
+
 app.all('/redirect', (req, res) => {
     const code = req.query.code;
     const returnedState = req.query.state;
-    if (csrf_string === returnedState) {
+    if (req.session.csrf_string === returnedState) {
         request.post(
             {
                 url:
@@ -54,13 +68,13 @@ app.all('/redirect', (req, res) => {
                         client_id: process.env.CLIENT_ID,
                         client_secret: process.env.CLIENT_SECRET,
                         code: code,
-                        redirect_uri: process.env.HOST + "/redirect",
-                        state: csrf_string
+                        redirect_uri: process.env.HOST  + "/redirect",
+                        state: req.session.csrf_string
                     })
             },
             (error, response, body) => {
-                access_token = qs.parse(body).access_token;
-                res.cookie('access_token', access_token, { maxAge: 900000, httpOnly: true });
+                req.session.access_token = qs.parse(body).access_token;
+                res.cookie('access_token', req.session.access_token, { maxAge: 900000, httpOnly: true });
                 res.redirect(process.env.CLIENT_HOST);
             }
         );
